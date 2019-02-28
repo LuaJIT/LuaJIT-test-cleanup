1846523c1aa3b734d5f07f2206f881fc948bacab
Siddhesh Poyarekar <siddhesh@gotplt.org>
2019-02-28 17:39:53 +0000

    Add scaling support to k-nucleatide.lua
    Add loop to run the search multiple times.

--- bench/k-nucleotide.lua
+++ bench/k-nucleotide.lua
@@ -48,11 +48,15 @@ local function readseq()
   return string.upper(table.concat(lines, "", 1, ln))
 end
 
+local N = tonumber(arg and arg[1]) or 0
+
 local seq = readseq()
-frequency(seq, 1)
-frequency(seq, 2)
-count(seq, "GGT")
-count(seq, "GGTA")
-count(seq, "GGTATT")
-count(seq, "GGTATTTTAATT")
-count(seq, "GGTATTTTAATTTATAGT")
+for i=1,N do
+  frequency(seq, 1)
+  frequency(seq, 2)
+  count(seq, "GGT")
+  count(seq, "GGTA")
+  count(seq, "GGTATT")
+  count(seq, "GGTATTTTAATT")
+  count(seq, "GGTATTTTAATTTATAGT")
+end
