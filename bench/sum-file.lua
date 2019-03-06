4c93e362a9f663ef7803d2484416494e8e73e470
Siddhesh Poyarekar <siddhesh@sourceware.org>
2019-03-06 20:11:38 +0530

    Revert "Support scaling support in the sum-file"
    This reverts commit 158ad7e7f11665bf3f7a0a40f3097f07ce865d8b.

--- bench/sum-file.lua
+++ bench/sum-file.lua
@@ -1,12 +1,6 @@
-local N = tonumber(arg and arg[1]) or 0
-lines = io.lines()
 
 local sum = 0
-
-for i = 1,N do
-  for line in lines do
-    sum = sum + line
-  end
+for line in io.lines() do
+  sum = sum + line
 end
-
 io.write(sum, "\n")
